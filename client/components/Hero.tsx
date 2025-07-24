d29410835986f316477fb7c7a0cf5ac70d561b19
Márcio Ferreira <161401869+euuCode@users.noreply.github.com>
2025-07-24 17:16:55 -0300

    Update Hero.tsx

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -6,7 +6,7 @@ export function Hero() {
       {/* Background Image with Advanced Overlay */}
       <div className="absolute inset-0 flex flex-col">
         <img
-          src="https://i.imgur.com/wtRUV9W.jpeg"
+          src="https://cdn.builder.io/api/v1/image/assets%2Ff4640e8889504e459e0229464c3c7d21%2F1564bbb3f4a24c80bb308de19336e863?format=webp&width=800"
           alt="Escritório Santo e Soares Advocacia"
           className="w-full h-full object-cover object-top scale-105 transition-transform duration-700 hover:scale-110"
         />
@@ -17,7 +17,7 @@ export function Hero() {
           className="absolute inset-0 professional-gradient backdrop-blur-[1px]"
           style={{
             backgroundImage:
-              "url (https://i.imgur.com/wtRUV9W.jpeg)",
+              "url (https://cdn.builder.io/api/v1/image/assets%2Ff4640e8889504e459e0229464c3c7d21%2F1564bbb3f4a24c80bb308de19336e863?format=webp&width=800)",
             backgroundRepeat: "no-repeat",
             backgroundPosition: "center",
             backgroundSize: "cover",
